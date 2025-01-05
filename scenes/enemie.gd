f7cae3380a248a8203cf58818427866b9933ccb2
KbraDev <ltovar.dev@gmail.com>
2025-01-04 18:16:29 -0600

    barra de vida de enemigo y tamano de area terminados

--- scenes/enemie.gd
+++ scenes/enemie.gd
@@ -1,15 +1,27 @@
 extends CharacterBody2D
 
 var health = 100
+@export var max_health = 100
+
+@onready var health_bar = $HealthBar
 
 func _ready() -> void:
-	print("Enemigo listo con vida: ", health)
+	health = max_health
+	update_health_bar()
 
 func take_damage(amount: int) -> void:
 	health -= amount
-	print("Enemigo recibió daño. Vida restante: ", health)
-	if health <= 0:
+	health = clamp(health, 0, max_health)
+	update_health_bar()
+	health_bar.visible = true
+	
+	# Ocultar la barra después de un tiempo
+	if health == 0:
 		die()
+
+func update_health_bar() -> void:
+	if health_bar:
+		health_bar.value = health
 
 func die() -> void:
 	print("Enemigo derrotado")
