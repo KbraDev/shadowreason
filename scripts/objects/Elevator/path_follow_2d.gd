ed2071f13a455d293dcfa7fdf16ec1c35f21845a
KbraDev <ltovar.dev@gmail.com>
2025-03-12 17:25:29 -0600

    nuevo elevador agregado

--- scripts/objects/Elevator/path_follow_2d.gd
+++ scripts/objects/Elevator/path_follow_2d.gd
@@ -1,19 +1,20 @@
 extends PathFollow2D
 
 @export var speed: float = 100.0
-var active: bool = false # si el jugador no esta en elevador
+var active: bool = false # si el jugador no esta en el elevador
 
 func _process(delta: float) -> void:
-	if active:
+	if active: 
 		progress += speed * delta
-	elif progress > 0: # si no hay jugador y el elevador no esta en la base, baja
+	elif progress > 01: # Si no hay jugador y el elevador no esta en su punto de inicio debe bajar
 		progress -= speed * delta
 
-func _on_area_2d_body_entered(body: Node2D) -> void:
+
+func _on_path_start_body_entered(body: Node2D) -> void:
 	if body.is_in_group("jugador"):
 		active = true
 
 
-func _on_area_2d_body_exited(body: Node2D) -> void:
+func _on_path_start_body_exited(body: Node2D) -> void:
 	if body.is_in_group("jugador"):
 		active = false
