f7a8bf6d17ae2167f3bb1e647f64d48582f84768
KbraDev <ltovar.dev@gmail.com>
2025-03-18 20:53:40 -0600

    nuevo background

---
+++ scripts/elevatorFollow2W.gd
@@ -0,0 +1,19 @@
+extends PathFollow2D
+
+@export var speed: float = 80.0
+var active: bool = false # si el jugador no esta en el elevador
+
+
+func _process(delta: float) -> void:
+	if active: 
+		progress += speed * delta
+	elif progress > 01: # Si no hay jugador y el elevador no esta en su punto de inicio debe bajar
+		progress -= speed * delta
+		
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.is_in_group("jugador"):
+		active = true
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	if body.is_in_group("jugador"):
+		active = false
